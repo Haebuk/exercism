872657361ed2cbc3d1558ce7cebbca229627ec13
Jaesung Ryu <kade@afidev.com>
2022-09-29 11:19:45 +0900

    method

---
+++ go/elons-toys/elons_toys.go
@@ -0,0 +1,29 @@
+package elon
+
+import "fmt"
+
+// TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	remainBattery := c.battery - c.batteryDrain
+	if remainBattery >= 0 {
+		c.battery = remainBattery
+		c.distance += c.speed
+	}
+	fmt.Printf("car is now %v", c)
+}
+
+// TODO: define the 'DisplayDistance() string' method
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
+
+// TODO: define the 'DisplayBattery() string' method
+func (c *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c *Car) CanFinish(trackDistance int) bool {
+	moveCount := c.battery / c.batteryDrain
+	return moveCount*c.speed >= trackDistance
+}
